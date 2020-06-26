998063600eaef617fd100a6e4e4323bdfb1f1cc2
Karol Gutiérrez <karo98gutierrez.q@gmail.com>
2020-06-25 20:59:16 -0600

    Se agrega prueba2Controllers

---
+++ ProyectoProgra6/Controllers/prueba2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgra6.Controllers
+{
+    public class prueba2Controller : Controller
+    {
+        // GET: prueba2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
