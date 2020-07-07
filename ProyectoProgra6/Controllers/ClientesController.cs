c82b34c7822dac81ba8768b6d6b44b62d342128d
JOSE ANDRES DELGADO ELIZONDO <116840414@castrocarazo.ac.cr>
2020-07-06 18:32:33 -0600

    Agrega controlador Cliente, se instala paquete Entity Framework

---
+++ ProyectoProgra6/Controllers/ClientesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoProgra6.Models;
+
+namespace ProyectoProgra6.Controllers
+{
+    public class ClientesController : Controller
+    {
+        progra6bdEntities modeloBD = new progra6bdEntities();
+        // GET: Clientes
+        /// <summary>
+        /// Metodo que se invoca al ingresar la direccion el navegador
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ClientesLista()
+        {
+            ///crear la variable que contiene los registros obtenidos
+            ///al invocar al procedimiento almacenado
+            List<sp_RetornaCliente_Result> modeloVista =
+                new List<sp_RetornaCliente_Result>();
+            ///asignar a la variable el resultado de "llamar al procedimiento
+            ///almacenado"
+            modeloVista = this.modeloBD.sp_RetornaCliente("", "", "").ToList();
+            //enviar a la vista el modelo
+            return View(modeloVista);
+        }
+    }
+}
